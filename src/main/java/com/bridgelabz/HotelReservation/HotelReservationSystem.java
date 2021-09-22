c99c7db3d7b7ed1634e65ebf32823d944d230b8d
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-22 09:40:21 +0530

    Added: Calculating the cheapest hotel

--- src/main/java/com/bridgelabz/HotelReservation/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservation/HotelReservationSystem.java
@@ -1,32 +1,65 @@
 package com.bridgelabz.HotelReservation;
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class HotelReservationSystem {
     String hotel;
     String customer;
+    HotelReservationSystem(){
+        hotel = " ";
+        customer = " ";
+    }
     HotelReservationSystem(String hotel,String customer){
         this.hotel = hotel;
         this.customer = customer;
     }
-    void hotelDetails(){
-        if (hotel == "LakeWood"){
+    void hotelDetails() {
+        if (hotel == "LakeWood") {
             int rate = 110;
-            System.out.println("Rate of Regular Customers of LakeWood Hotel:"+rate);
+            System.out.println("Rate of Regular Customers of LakeWood Hotel:" + rate);
         }
-        if (hotel == "BridgeWood"){
+        if (hotel == "BridgeWood") {
             int rate = 160;
-            System.out.println("Rate of Regular Customers of BridgeWood Hotel:"+rate);
+            System.out.println("Rate of Regular Customers of BridgeWood Hotel:" + rate);
         }
-        if (hotel == "RidgeWood"){
+        if (hotel == "RidgeWood") {
             int rate = 220;
-            System.out.println("Rate of Regular Customers of RidgeWood Hotel:"+rate);
+            System.out.println("Rate of Regular Customers of RidgeWood Hotel:" + rate);
         }
     }
-    public static void main(String[] args){
+    void cheapestHotel(){
+        int count=0;
+        count = (count+2);
+        int LakeWood_Rate = 110, BridgeWood_Rate = 160, RidgeWood_Rate = 220;
+        LakeWood_Rate = (LakeWood_Rate*count);
+        BridgeWood_Rate = (BridgeWood_Rate*count);
+        RidgeWood_Rate = (RidgeWood_Rate*count);
+        if (LakeWood_Rate<BridgeWood_Rate && LakeWood_Rate<RidgeWood_Rate){
+            System.out.println("LakeWood");
+            System.out.println(LakeWood_Rate);
+        }
+        if (BridgeWood_Rate<LakeWood_Rate && BridgeWood_Rate<RidgeWood_Rate){
+            System.out.println("BridgeWood");
+            System.out.println(BridgeWood_Rate);
+        }
+        if (RidgeWood_Rate<BridgeWood_Rate && RidgeWood_Rate<LakeWood_Rate){
+            System.out.println("RidgeWood");
+            System.out.println(RidgeWood_Rate);
+        }
+    }
+    public static void main(String[] args) throws Exception {
         HotelReservationSystem hotel1 = new HotelReservationSystem("LakeWood","Regular");
         HotelReservationSystem hotel2 = new HotelReservationSystem("BridgeWood","Regular");
         HotelReservationSystem hotel3 = new HotelReservationSystem("RidgeWood","Regular");
         hotel1.hotelDetails();
         hotel2.hotelDetails();
         hotel3.hotelDetails();
+        String sDate1="22-09-2021";
+        String sDate2="23-09-2021";
+        Date date1=new SimpleDateFormat("dd-MM-yyyy").parse(sDate1);
+        Date date2=new SimpleDateFormat("dd-MM-yyyy").parse(sDate2);
+        System.out.println(sDate1+"\t"+date1);
+        System.out.println(sDate2+"\t"+date2);
+        HotelReservationSystem hotel = new HotelReservationSystem();
+        hotel.cheapestHotel();
     }
 }
--- src/test/java/com/bridgelabz/HotelReservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/HotelReservation/HotelReservationTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 
 public class HotelReservationTest {
     HotelReservationSystem obj = new HotelReservationSystem("LakeWood","Regular");
-
+    HotelReservationSystem cheap_hotel = new HotelReservationSystem();
     @Test
     public void toCheck_HotelDetails() {
         obj.hotelDetails();
+    }
+    @Test
+    public void toCheck_CheapHotel() {
+        cheap_hotel.cheapestHotel();
     }
 }
